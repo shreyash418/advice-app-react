2e5e9d21d43a26d061c3ebf18c0c6a88bb21c05e
shreyash shukla <98917970+shreyash418@users.noreply.github.com>
2023-07-07 14:46:36 +0530

    Create Trans.js

---
+++ src/Trans.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import './App.css';
+const Trans = () => {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false
+      },
+      "google_translate_element"
+    );
+  };
+  useEffect(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+  return (
+    <>
+      <div id="google_translate_element"></div>
+     
+    </>
+  );
+};
+
+export default Trans;
